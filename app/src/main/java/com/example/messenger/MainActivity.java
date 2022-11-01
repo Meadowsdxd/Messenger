d2ca8fa3ca26bed92558d04897399c941ffc43a3
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-11-01 22:38:53 +0200

    fill code navigation menu

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -1,36 +1,61 @@
 package com.example.messenger;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private final List<Messages> items = new ArrayList<>();
     private final RecyclerView.Adapter adapter = new MainAdapter(this.items);
     EditText edit;
     MainAdapter myCardAdapter;
     RecyclerView recyclerView;
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edit = this.findViewById(R.id.editText);
-         recyclerView = findViewById(R.id.RV);
+
+        recyclerView = findViewById(R.id.RV);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+    ImageView imageView=findViewById(R.id.ImageViewMenu);
+         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer , R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        imageView.setOnClickListener(m->{
+            drawer.openDrawer(GravityCompat.START);
+        });
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
     public void add(View view) {
@@ -39,5 +64,17 @@ public class MainActivity extends AppCompatActivity {
         edit.setText("");
         adapter.notifyItemInserted(this.items.size() - 1);
     }
-    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.About_item){drawer.openDrawer(GravityCompat.START);}
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+}
+}
+
 
