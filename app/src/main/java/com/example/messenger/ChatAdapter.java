5c4f5edcf3c6c202b1e244647d705484ae832ba1
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-11-05 19:21:02 +0200

    Create chat adapter

---
+++ app/src/main/java/com/example/messenger/ChatAdapter.java
@@ -0,0 +1,56 @@
+package com.example.messenger;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public final class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>  {
+    private  List<Messages> items;
+
+    public ChatAdapter(List<Messages> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int index) {
+        return new RecyclerView.ViewHolder(
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.itemschat, parent, false)
+        ) {};
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int index) {
+        TextView name = holder.itemView.findViewById(R.id.name);
+        name.setText(String.format("%s",this.items.get(index).getName()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.items.size();
+    }
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") int position, @NonNull List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("ResourceType")
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(v.getContext(), String.valueOf(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
+
